fff160855ced9e31599dfee25117015e3d2b1013
Anuj61-t <anuj.mistry02@gmail.com>
2023-01-20 23:22:55 +0530

    add dotenv file and add port

--- authentication/index.js
+++ authentication/index.js
@@ -1,5 +1,6 @@
 const app = require("./app");
+const {PORT} = process.env
 
-app.listen(4000,()=>{
-    console.log(`Server is running on port 4000`);
+app.listen(PORT,()=>{
+    console.log(`Server is running on port ${PORT}`);
 })
